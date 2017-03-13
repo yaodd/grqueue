f022d4b28ff66b5fc4e12c64f45b906b279f7c29
yaodunding <yaodunding@kingsoft.com>
2017-03-13 16:29:33 +0800

    first commit

---
+++ goroutinequeue.go
@@ -0,0 +1,76 @@
+package grqueue
+
+import (
+	"sync"
+)
+
+type GoroutineQueue struct {
+	Number int
+	Total  int
+
+	tasks             chan func() interface{}
+	task_end_callback func(result interface{})
+	finish_callback   func()
+}
+
+func NewGoroutineQueue(number int, total int) *GoroutineQueue {
+	queue := &GoroutineQueue{
+		tasks:  make(chan func() interface{}, total),
+		Number: number,
+		Total:  total}
+	return queue
+}
+
+//开始执行task
+func (queue *GoroutineQueue) Start() {
+	defer close(queue.tasks)
+	//加锁，锁的数量是tasks的数量
+	wg.Add(len(queue.tasks))
+	for i := 0; i < queue.Number; i++ {
+
+		//分number个routine执行task
+		go queue.work()
+	}
+
+	//等待routine执行完毕
+	wg.Wait()
+
+	//所有task完毕，若finish回调函数存在则执行则回调
+	if queue.finish_callback != nil {
+		queue.finish_callback()
+	}
+}
+
+var wg sync.WaitGroup
+
+func (queue *GoroutineQueue) work() {
+
+	for {
+
+		//不断取出task执行，直到chan关闭
+		task, ok := <-queue.tasks
+		if !ok {
+			break
+		}
+		res := task()
+
+		//完成一个task立即回调
+		if queue.task_end_callback != nil {
+			queue.task_end_callback(res)
+		}
+
+		//每执行完一个task，解锁一次
+		wg.Done()
+	}
+
+}
+
+func (queue *GoroutineQueue) AddTask(task func() interface{}) {
+	queue.tasks <- task
+}
+func (queue *GoroutineQueue) SetFinishCallback(callback func()) {
+	queue.finish_callback = callback
+}
+func (queue *GoroutineQueue) SetTaskEndCallback(callback func(result interface{})) {
+	queue.task_end_callback = callback
+}
